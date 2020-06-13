6363fe3593cc442d746fb7ee29f4a8cee1b3ee0b
Raslan Rauff <raslan1994@gmail.com>
2020-06-13 16:59:28 +0530

    added logger

---
+++ appLogger.js
@@ -0,0 +1,22 @@
+import { getAppStage, APP_STAGE } from "./appStage";
+
+const LOG_LEVEL = {
+  debug: "debug",
+  error: "error",
+  info: "info",
+};
+
+const writeLogTag = (tag) => `[${tag}] -> `;
+
+function writeLog(tag, data, level = LOG_LEVEL.debug) {
+  const appStage = getAppStage();
+  if (level === LOG_LEVEL.debug && appStage === APP_STAGE.debug) {
+    console.log(writeLogTag(tag), data);
+  } else if (level === LOG_LEVEL.error && appStage === APP_STAGE.debug) {
+    console.error(writeLogTag(tag), data);
+  } else {
+    console.log(writeLogTag(tag), data);
+  }
+}
+
+export { LOG_LEVEL, writeLog };
